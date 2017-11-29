365d904060a12fb5ee847bf9e78008d948ba05c1
xueyu <xueyu@DESKTOP-NN3K5TR>
2017-11-29 22:57:46 +0800

    test22:57

--- src/main/java/com/test/controller/Test.java
+++ src/main/java/com/test/controller/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		Date date = new Date();
-	    long times = date.getTime();//Ê±¼ä´Á
+	    long times = date.getTime();//æ—¶é—´æˆ³
 	    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	    String dateString = formatter.format(date);
 	    System.out.println(dateString);
